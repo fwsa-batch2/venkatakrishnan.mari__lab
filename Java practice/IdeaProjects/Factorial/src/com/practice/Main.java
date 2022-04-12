10812307d04a64334eb7a226182da57b81b7ce71
Venkatakrishnan434 <venkatakrishnan434@gmail.com>
2022-04-12 12:39:23 +0530

    added new files

---
+++ Java practice/IdeaProjects/Factorial/src/com/practice/Main.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int fact = 1;
+        int number = 6;
+        for(int a=1; a<=number;a++) {
+            fact = fact*a;
+
+        }
+        System.out.print("Factorial is " +fact);
+        }
+    }
+
+
---
+++ Java practice/IdeaProjects/Main.java/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int rollNum = 6040425;
+        long phNum = 9361041092L;
+        String name = "VenkataKrishnan";
+        char gender = 'M';
+        boolean workStatus = false;
+        float mark = 98.5f;
+        double temp = 90.34;
+        System.out.println(rollNum);
+        System.out.println(phNum);
+        System.out.println(name);
+        System.out.println(gender);
+        System.out.println(workStatus);
+
+    }
+
+    }
+
