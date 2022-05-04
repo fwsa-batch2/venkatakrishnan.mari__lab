f5bdc5db4f88ddf9a5e88075ccea7cc743d7c017
Venkatakrishnan434 <venkatakrishnan434@gmail.com>
2022-05-04 17:59:59 +0530

    added new files

---
+++ Java/GarbageCollector.java
@@ -0,0 +1,12 @@
+public class GarbageCollector {
+    public static void main(String[] args) {
+        GarbageCollector obj1 = new GarbageCollector();
+        GarbageCollector obj2 = new GarbageCollector();
+        GarbageCollector obj3 = new GarbageCollector();
+
+        
+        System.out.println(obj1);
+        System.out.println(obj2);
+        System.out.println(obj3);
+    }
+}
